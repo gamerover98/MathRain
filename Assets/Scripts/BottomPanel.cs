d76e167bd5effd7adf1f86cb5da2040162091ddf
gamerover98 <gamerover98@hotmail.it>
2024-03-03 12:24:11 +0100

    The InputField is focused on startup

--- Assets/Scripts/BottomPanel.cs
+++ Assets/Scripts/BottomPanel.cs
@@ -1,7 +1,15 @@
+using TMPro;
 using UnityEngine;
 
 public class BottomPanel : MonoBehaviour
 {
+    [SerializeField] private TMP_InputField inputField;
+
+    private void Start()
+    {
+        inputField.Select();
+    }
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         other.gameObject.TryGetComponent<Drop>(out var drop);
